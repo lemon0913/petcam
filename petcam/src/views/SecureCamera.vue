a8a8c5e7207cec630439f7ef21405f6011566bcb
shpark951 <shpark95145@gmail.com>
2021-10-15 17:01:11 +0900

    upload snapshot
    upload snapshot

--- petcam/src/views/SecureCamera.vue
+++ petcam/src/views/SecureCamera.vue
@@ -3,7 +3,7 @@
         <page-title icon="mdi-video">실시간 카메라</page-title>
 
         
-        <remote-camera url="http://192.168.0.50:8000/mjpeg/stream/" max-width="640"></remote-camera>
+        <remote-camera url="http://192.168.0.9:8000/mjpeg/stream/" max-width="640"></remote-camera>
         <!-- <remote-camera url="https://picsum.photos/seed/picsum/640/480" max-width="640"></remote-camera> -->
         
     </div>
--- petcam_django/mysite/settings.py
+++ petcam_django/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9@892asun9c9py6r5+5g_9xmzrzs%g$o5^cp3d4ys_42)7*1an
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -118,12 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS= [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
